e6300851819a879848e87cad701c55405dba8093
Tyler-Osborne <Rainbowdash!1989>
2021-10-08 20:05:50 -0500

    Initial Commit

---
+++ Kompressor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Nukleah
+{
+    public static class Kompressor
+    {
+        public static List<int> Alphabet { get; set; } = new List<int> {0, 1, 2};
+    }
+}
